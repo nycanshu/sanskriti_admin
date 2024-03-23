afcd1b75d608ca54f946e21e131f435ad35b92e1
Himanshu <112685847+nycanshu@users.noreply.github.com>
2024-03-23 14:16:54 +0530

    sanskriti admin

---
+++ android/app/src/main/kotlin/com/example/sanskriti_admin/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sanskriti_admin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ web/firebase-config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC4JnP6PYZ6UoELCM_dPSUP--IRHIYzzQ4",
+    authDomain: "sanskriti-by-okay-anshu.firebaseapp.com",
+    databaseURL: "https://sanskriti-by-okay-anshu-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "sanskriti-by-okay-anshu",
+    storageBucket: "sanskriti-by-okay-anshu.appspot.com",
+    messagingSenderId: "43440781583",
+    appId: "1:43440781583:web:814614fd4c58535a1aa67a",
+    measurementId: "G-R5LD38S44N"
+  };
